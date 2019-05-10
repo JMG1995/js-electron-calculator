eb52c57dcb6292ac1f3d905863c0a1131211c3c0
Joseph <jmgrant1995@gmail.com>
2019-05-10 16:43:35 +0800

    add function to check type of button pressed

--- js/app.js
+++ js/app.js
@@ -2,9 +2,28 @@ const getKey = () => {
   const keys = document.querySelectorAll("button");
   let result = document.querySelector(".screen__result");
   keys.forEach(key => {
-    return key.addEventListener("click", () => {
-      result.innerHTML = key.innerHTML;
-      console.log(key.innerHTML);
+    key.addEventListener("click", e => {
+      const btn = e.target;
+      const action = btn.dataset.action;
+      if (!action) {
+        console.log("number key");
+      } else if (
+        action === "divide" ||
+        action === "add" ||
+        action === "subtract" ||
+        action === "multiply"
+      ) {
+        console.log("operator key");
+      } else if (action === "decimal") {
+        console.log("decimal key");
+      } else if (action === "equals") {
+        console.log("equals key");
+      }
+
+      if (key === "=") {
+        return null;
+      }
+      return (result.innerHTML = key.innerHTML);
     });
   });
 };
