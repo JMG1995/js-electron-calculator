a12e829bd7453ca121b40b7e7030b0e126dc9697
Joseph <jmgrant1995@gmail.com>
2019-05-20 13:15:45 +0800

    split keys into separate functions

--- js/app.js
+++ js/app.js
@@ -24,11 +24,48 @@ const numKey = (prevKey, item) => {
     prevKey == "equals" ||
     prevKey === ""
   ) {
-    result.textContent = item.textContent;
+    return (result.textContent = item.textContent);
   } else {
-    result.textContent += item.textContent;
     prevKey = "number";
+    return (result.textContent += item.textContent);
   }
+};
+
+const operatorKey = (prevKey, action) => {
+  if (prevKey == "operator" || prevKey == "equals") {
+    return result.textContent;
+  }
+  const firstVal = calculator.dataset.firstVal;
+  const operator = calculator.dataset.operator;
+  const secondval = result.textContent;
+  // hitting an operator key will sum the previous 2 numbers
+  if (firstVal && operator) {
+    result.textContent = calculate(firstVal, operator, secondval);
+  }
+  prevKey = "operator";
+  calculator.dataset.firstVal = result.textContent;
+  calculator.dataset.operator = action;
+};
+
+const decimal = (prevKey, item) => {
+  // clicking decimal results in "0." if previous button was operator
+  if (prevKey == "operator") {
+    result.textContent = "0";
+  }
+  result.textContent += item.textContent;
+  return (prevKey = "decimal");
+};
+
+const equals = prevKey => {
+  if (prevKey == "equals" || prevKey == "decimal" || prevKey == "operator") {
+    return result.textContent;
+  }
+  prevKey = "equals";
+  const firstVal = calculator.dataset.firstVal;
+  const operator = calculator.dataset.operator;
+  const secondval = result.textContent;
+
+  return (result.textContent = calculate(firstVal, operator, secondval));
 };
 
 const getKey = () => {
@@ -36,17 +73,8 @@ const getKey = () => {
     key.addEventListener("click", e => {
       const btn = e.target;
       const action = btn.dataset.action;
+      console.log(action);
       let previousKeyType = calculator.dataset.previousKeyType;
-
-      // if (result.textContent.length > 16) {
-      //   return undefined;
-      // } else if (result.textContent.length > 13) {
-      //   screen.style.fontSize = "1.4em";
-      // } else if (result.textContent.length > 11) {
-      //   screen.style.fontSize = "1.6em";
-      // } else if (result.textContent.length > 9) {
-      //   screen.style.fontSize = "2em";
-      // }
       // check type of key pressed and perform appropriate function
       if (!action) {
         numKey(previousKeyType, key);
@@ -56,44 +84,25 @@ const getKey = () => {
         action === "subtract" ||
         action === "multiply"
       ) {
-        if (previousKeyType == "operator" || previousKeyType == "equals") {
-          return result.textContent;
-        }
-        const firstVal = calculator.dataset.firstVal;
-        const operator = calculator.dataset.operator;
-        const secondval = result.textContent;
-        // hitting an operator key will sum the previous 2 numbers
-        if (firstVal && operator) {
-          result.textContent = calculate(firstVal, operator, secondval);
-        }
-        previousKeyType = "operator";
-        calculator.dataset.firstVal = result.textContent;
-        calculator.dataset.operator = action;
+        operatorKey(previousKeyType, action);
       } else if (
         // adds decimal if does not exist
         action === "decimal" &&
         !result.textContent.includes(".")
       ) {
-        // clicking decimal results in "0." if previous button was operator
-        if (previousKeyType == "operator") {
-          result.textContent = "0";
-        }
-        result.textContent += key.textContent;
-        previousKeyType = "decimal";
+        decimal(previousKeyType, key);
       } else if (action === "equals") {
-        if (
-          previousKeyType == "equals" ||
-          previousKeyType == "decimal" ||
-          previousKeyType == "operator"
-        ) {
-          return result.textContent;
-        }
-        previousKeyType = "equals";
-        const firstVal = calculator.dataset.firstVal;
-        const operator = calculator.dataset.operator;
-        const secondval = result.textContent;
+        equals(previousKeyType);
+      }
 
-        result.textContent = calculate(firstVal, operator, secondval);
+      if (result.textContent.length > 16) {
+        result.textContent;
+      } else if (result.textContent.length > 13) {
+        screen.style.fontSize = "1.4em";
+      } else if (result.textContent.length > 11) {
+        screen.style.fontSize = "1.6em";
+      } else if (result.textContent.length > 9) {
+        screen.style.fontSize = "2em";
       }
 
       // end check
